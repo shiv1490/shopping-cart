da3932bad7f141585f24faeecb3b5f0269af897a
shivam <shivam@github.com>
2019-02-18 22:13:57 +1100

    fix: name change

--- src/hoc/Layout.js
+++ src/hoc/Layout.js
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "../containers/Homepage";
 import ProductDetails from "../components/productDetails/productDetails";
-import Cart from "../containers/cart";
+import Cart from "../containers/Cart";
 import Header from "../components/header/header";
 
 const Layout = props => {
